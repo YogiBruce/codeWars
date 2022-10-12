c8cb55cb7218d1b387d8f3f989399cb8d1186759
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2022-10-11 20:23:39 -0700

    more beasts

--- 8kyu/feastOfBeasts.js
+++ 8kyu/feastOfBeasts.js
@@ -1,11 +1,20 @@
 feast = (beast, dish) => beast[0] == dish[0] && beast[beast.length-1] == dish[dish.length-1]
 
-//feast = (beast, dish) => beast[0]===dish[0] && beast.slice(-1)===dish.slice(-1)
+feast = (beast, dish) => beast[0]===dish[0] && beast.slice(-1)===dish.slice(-1)
 
-// function feast(beast,dish){
-//   if ((beast[0] == dish[0]) && (beast[beast.length-1] == dish[dish.length-1])){
-//     return true
-//   } else {
-//     return false
-//   }
-// }
+function feast(beast,dish){
+  if ((beast[0] == dish[0]) && (beast[beast.length-1] == dish[dish.length-1])){
+    return true
+  } else {
+    return false
+  }
+}
+
+
+let feast = (a, b) => a[0] + [...a].pop() == b[0] + [...b].pop()
+
+
+feast = (a, b) =>  a[0] == b[0] && a.substr(-1) == b.substr(-1)
+
+
+const feast = (b, d) => `${b[0]}${b[b.length - 1]}` === `${d[0]}${d[d.length - 1]}`
