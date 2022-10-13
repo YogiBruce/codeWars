ac5febd748e8b442c16337a102036ec5ca9d65d7
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2022-10-12 17:07:05 -0700

    getVolumeOfCuboid

---
+++ 8kyu/volumeOfCuboid.js
@@ -0,0 +1,25 @@
+const Kata = {
+    getVolumeOfCuboid: (l, w, h) => l * w * h
+}
+
+
+class Kata {
+    static getVolumeOfCuboid(l, w, h) {
+        return l * w * h;
+    }
+};
+
+
+class Kata {
+    static getVolumeOfCuboid(l, w, h) {
+        return l * w * h
+    }
+}
+
+
+class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+        let vol = length * width * height
+        return vol
+    }
+}
