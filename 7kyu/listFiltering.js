4d5ebcba5c90ca2558c4c58629499297f5e76586
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2022-10-12 17:09:21 -0700

    listFiltering

---
+++ 7kyu/listFiltering.js
@@ -0,0 +1,9 @@
+//write a function that returns an array with numbers filtered out
+
+const filter_list = l => l.filter(n => typeof n == 'number')
+
+function filter_list(l) {
+    return l.filter(n => typeof n == 'number')
+  }
+
+  const filter_list = l => l.filter(n => typeof n == 'number')
