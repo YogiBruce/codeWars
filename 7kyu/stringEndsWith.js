459ecb262116040aac0fcc0d6f838fe14cadc287
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2022-10-15 18:47:45 -0700

    endsWith more solutions

--- 7kyu/stringEndsWith.js
+++ 7kyu/stringEndsWith.js
@@ -1,9 +1,26 @@
-function solution(str, ending){
-    if(str.endsWith(ending)){
-      return true
-    }else {
-      return false
+//Using if function
+function solution(str, ending) {
+    if (str.endsWith(ending)) {
+        return true
+    } else {
+        return false
     }
-  }
+}
 
-  console.log(solution("run it back", "ack"))
+console.log(solution("run it back", "ack"))
+
+
+
+//simple solution
+function solution(str, ending){
+    return str.endsWith(ending);
+}
+
+console.log(solution("run it back", "ack"))
+
+
+
+//Arrow function
+const solution = (str,ending) => str.endsWith(ending);
+
+console.log(solution("run it back", "ack"))
