9b1b91a9cd5fae4b1c47d0fb8bd0cb1e94ff602b
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2022-10-15 18:51:40 -0700

    more solves endsWith

--- 7kyu/stringEndsWith.js
+++ 7kyu/stringEndsWith.js
@@ -6,21 +6,26 @@ function solution(str, ending) {
         return false
     }
 }
-
 console.log(solution("run it back", "ack"))
-
 
 
 //simple solution
 function solution(str, ending){
     return str.endsWith(ending);
 }
-
 console.log(solution("run it back", "ack"))
 
 
 
 //Arrow function
 const solution = (str,ending) => str.endsWith(ending);
+//shorthand
+const solution = (s, t) => s.endsWith(t);
+
+console.log(solution("run it back", "ack"))
+
+
+//Sting slice method
+const solution = (str, ending) => str.slice(0-ending.length) === ending;
 
 console.log(solution("run it back", "ack"))
