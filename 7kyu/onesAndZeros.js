60e5b4952356206607194f48a60e1ad5814144f9
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2022-10-15 18:31:17 -0700

    ones update

--- 7kyu/onesAndZeros.js
+++ 7kyu/onesAndZeros.js
@@ -7,3 +7,5 @@ const binaryArrayToNumber = arr => {
   // binaryToNumber([0, 1, 1, 0]) should return 6
   // binaryToNumber([1, 1, 1, 1]) should return 15
   // binaryToNumber([0, 0, 0, 1]) should return 1
+
+  console.log(binaryArrayToNumber([...]))
