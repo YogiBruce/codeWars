1618ba8ced438625f36489b84412d76194e7ce2f
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2022-10-15 18:17:07 -0700

    ones and zeros

---
+++ 7kyu/onesAndZeros.js
@@ -0,0 +1,9 @@
+const binaryArrayToNumber = arr => {
+    return parseInt(arr.join(""), 2)
+  };
+  
+  
+  //Examples:
+  // binaryToNumber([0, 1, 1, 0]) should return 6
+  // binaryToNumber([1, 1, 1, 1]) should return 15
+  // binaryToNumber([0, 0, 0, 1]) should return 1
