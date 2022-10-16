ce4912bfdd8d8f1a12852914151d4b7c40bb32ab
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2022-10-16 13:54:33 -0700

    for loop on howGood

--- 8kyu/howGoodAreYouReally.js
+++ 8kyu/howGoodAreYouReally.js
@@ -1,2 +1,11 @@
 //Arrow function and reduce method (implicit return)
 const betterThanAverage = (classPoints, yourPoints) => classPoints.reduce((prev,curr) => prev+curr)/classPoints.length < yourPoints
+
+//Classic function with for loop
+function betterThanAverage (classPoints, yourPoints){
+    let sum =0
+    for (let i=0; i<classPoints.length; i++){
+      sum += classPoints[i]
+    }
+    return sum/classPoints.length < yourPoints
+  }
