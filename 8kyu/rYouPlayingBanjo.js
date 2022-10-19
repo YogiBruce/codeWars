05638bb69340bd2f608dfd5a8540151efb40d8fc
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2022-10-18 21:11:16 -0700

    more solves

--- 8kyu/rYouPlayingBanjo.js
+++ 8kyu/rYouPlayingBanjo.js
@@ -9,3 +9,15 @@ function areYouPlayingBanjo(name) {
   }
 
   console.log(areYouPlayingBanjo("Alexandra"))
+
+  //Ternary?
+  function rBanjoPlayer(name){
+    return ((name[0]==="R")||(name[0]==="r") ? `${name} plays banjo` : `${name} does not play banjo`)
+  }
+
+  console.log(rBanjoPlayer("Robert"))
+
+  //Arrow function ternary
+  const ourBanjoPlayer = (name) => (name[0]==="R")||(name[0]==="r") ? `${name} plays banjo` : `${name} does not play banjo`
+
+  console.log(ourBanjoPlayer("rebecca"))
