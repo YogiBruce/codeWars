9365a88b1212200cae7d3aaa31ea62afbc357af6
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2022-10-21 18:23:27 -0700

    simpleMultiplication

---
+++ 8kyu/simpleMultiplication.js
@@ -0,0 +1,7 @@
+//If number is even multiply by 8, if odd multiply by 9
+
+const simpleMultiplication = (n) => n%2===0 ? n*8 : n*9
+
+//tests
+console.log(simpleMultiplication(4))
+console.log(simpleMultiplication(3))
