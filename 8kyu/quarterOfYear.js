3a1bcb82527735925248c921c2687081929f756a
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2022-11-03 18:55:35 -0700

    8kyu quarterOf2

--- 8kyu/quarterOfYear.js
+++ 8kyu/quarterOfYear.js
@@ -9,7 +9,13 @@ const quarterOf = (month) => {
     }else {
       return 4
     }
-    
-  }
+  };
+
+  //August
+  console.log(quarterOf(8))
 
 //Arrow function using Math.ceil
+const quarterOf = (m) => Math.ceil(m/3)
+
+//December
+consol.log(quarterOf(12))
