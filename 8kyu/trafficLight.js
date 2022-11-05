7198f80b33ec94bd598edad6f86c9c020df539d5
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2022-11-05 14:44:46 -0700

    8kyu trafficLight 2

--- 8kyu/trafficLight.js
+++ 8kyu/trafficLight.js
@@ -2,3 +2,7 @@ function updateLight(current) {
     return ({green: 'yellow', yellow: 'red', red: 'green'})
     [current]
   }
+
+  function updateLight(current) {
+    return current === 'yellow'? 'red': current === 'green' ? 'yellow' : 'green'
+  }
