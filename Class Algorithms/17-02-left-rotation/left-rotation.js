357c4b7f020f2b4285b5849f8e6041ffd0feeab4
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2022-12-10 11:56:50 -0800

    17-02-left-rotation

---
+++ Class Algorithms/17-02-left-rotation/left-rotation.js
@@ -0,0 +1,26 @@
+// Write a function that takes an array and a positive integer and "rotates" the array to the left by the integer
+// Modify the original array rather than returning a new one
+// There is no need to return from this function
+
+//Original solution
+const leftRotation = (arr, positions) => {
+    for (i=0; i<positions; i++){
+        arr.unshift(arr.pop())
+    }
+    return arr
+};
+
+//Provided solution
+const leftRotation = (arr, positions) => {
+    if (arr.length === 0) {
+      return;
+    }
+  
+    while (positions > 0) {
+      const first = arr.shift();
+  
+      arr.push(first);
+      console.log(arr)
+      positions--;
+    }
+  };
