29ebbc5f51083ac19461379e3d50895578759a8c
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2023-02-15 17:47:44 -0800

    7kyu sumCubes1

---
+++ 7kyu/sumCubes.js
@@ -0,0 +1,15 @@
+// Write a function that takes a positive integer n, sums all the cubed values from 1 to n (inclusive), and returns that sum.
+
+// Assume that the input n will always be a positive integer.
+
+// Using a while loop
+function sumCubes(n) {
+  let result = 0;
+  while (n > 0) {
+    // updates result with addition of iteration cubed
+    result += n ** 3;
+    //counts iterations to zero
+    n--;
+  }
+  return result;
+}
