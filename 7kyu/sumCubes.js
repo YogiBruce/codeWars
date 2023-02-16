08dec0b1453cdf88a757126ea2a5b08204916deb
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2023-02-15 17:48:30 -0800

    7kyu sumCubes2

--- 7kyu/sumCubes.js
+++ 7kyu/sumCubes.js
@@ -13,3 +13,11 @@ function sumCubes(n) {
   }
   return result;
 }
+
+// Using Array methods
+const sumCubes = (n) =>
+  Array.from({ length: n }, (_, index) => (index + 1) ** 3).reduce(
+    (acc, curr) => acc + curr
+  );
+
+
